fb77de376e1472320ec3c719202f6a51d5575f76
t3chgirl00 <keziah.odoi@turntabl.io>
2021-11-12 19:12:00 +0000

    Created tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,30 +1,11 @@
-import { useState } from "react";
+import Task from "./Task";
 
-const Tasks = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctor's Appointment",
-      day: "Feb 5th at 3:30 pm",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Meeting at School",
-      day: "Feb 5th at 1:30 pm",
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: "Food Shopping",
-      day: "Feb 5th at 2:30 pm",
-      reminder: false,
-    },
-  ]);
+const Tasks = ({tasks}) => {
+ 
   return (
     <>
       {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+        <Task key={task.id} task = {task}/>
       ))}
     </>
   );
