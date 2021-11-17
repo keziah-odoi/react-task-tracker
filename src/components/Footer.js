12c07cb53d257f3f0384c5d396a52302da9a9db1
t3chgirl00 <keziah.odoi@turntabl.io>
2021-11-17 18:32:46 +0000

    Keziah Odoi completed the task tracker.
    All errors within the task tracker fdg

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer>
-      <p>Copyright &copy; 2021 </p>
+      <p>Copyright &copy; Keziah Odoi 2021 </p>
       <a href="/about">About</a>
     </footer>
   );
